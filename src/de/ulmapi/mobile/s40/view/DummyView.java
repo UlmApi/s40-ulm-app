f7cb86e2e245d51980876dadd91003517991aa38
Benjamin Erb <mail@benjamin-erb.de>
2012-03-18 17:27:49 +0100

    test map

--- src/de/ulmapi/mobile/s40/view/DummyView.java
+++ src/de/ulmapi/mobile/s40/view/DummyView.java
@@ -2,15 +2,22 @@ package de.ulmapi.mobile.s40.view;
 
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.Item;
 import javax.microedition.lcdui.ItemCommandListener;
 import javax.microedition.lcdui.ItemStateListener;
+import javax.microedition.lcdui.List;
 import javax.microedition.lcdui.StringItem;
 
 import org.json.me.JSONException;
 import org.json.me.JSONObject;
+
+import com.nokia.maps.common.GeoCoordinate;
+import com.nokia.maps.map.MapCanvas;
+import com.nokia.maps.map.MapShapeType;
 
 import de.ioexception.me.http.HttpManager;
 import de.ioexception.me.http.HttpResponse;
@@ -51,6 +58,37 @@ public final class DummyView extends Form implements CommandListener, ItemComman
 		setItemStateListener(this);
 		
 		append(stringItem);
+		
+//		zr
+//		String[] strings = {"a","b"};
+//		
+//		List list = new List("foo", List.IMPLICIT, strings, imgs);
+//		
+		
+		try{
+			String[] items = new String[]{"Item 1", "Item 2", "Item 3"};
+			Image[] imgs = {Image.createImage("/stationListView.png"),Image.createImage("/stationListView.png"),Image.createImage("/stationListView.png")};
+//			CanvasList myCanvas = new CanvasList("Test canvas", items, imgs);
+			List list = new List("foo", List.IMPLICIT, items, imgs);
+//			Display.getDisplay(midlet).setCurrent(list);	
+			
+			Display display = Display.getDisplay(midlet);
+	        MapCanvas mapCanvas = new MapCanvas( display  );
+	        display.setCurrent(mapCanvas);
+	        
+	        mapCanvas.getMapDisplay().addMapObject(mapCanvas.getMapFactory().createStandardMarker(new GeoCoordinate(49, 10, 0), 10, "test", MapShapeType.baloon));
+			
+			GeoCoordinate coord = new GeoCoordinate( 10, 10, 0 ); 
+			
+			Display.getDisplay(midlet).setCurrent(mapCanvas);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+//		append(new StationItem("asd", "asfdasd", 21));
+//		append(new StationItem("asd", "asfdasd", 21));
+
 	}
 
 	public void itemStateChanged(Item item) {
