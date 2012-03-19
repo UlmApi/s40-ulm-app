c786dbae526ced67fce26b726aea9a71a3de55ca
cmichi <mich@elmueller.net>
2012-03-19 15:20:31 +0100

    added script for importing postboxes into couchdb

---
+++ parse-osm/insert-into-couchdb.js
@@ -0,0 +1,40 @@
+/* simple script for inserting our json with postboxes into couchdb */
+var request = require('request');
+var fs = require('fs');
+var crypto = require('crypto');
+var credentials = require('./credentials.js');
+
+var json = fs.readFileSync('./postboxes.json', 'utf8');
+var json = JSON.parse(json);
+
+
+function insert(entry) {
+	request(
+	  { method: 'PUT'
+	  , uri:  'http://daten.ulmapi.de/postkaesten/' + entry.id
+	,headers: 	{'Authorization': 'Basic ' + credentials.auth}
+	  , multipart: 
+	    [ { 'content-type': 'application/json'
+	      ,  body: JSON.stringify(entry)
+	      }
+	    ] 
+	  }
+	, function (error, response, body) {
+	    if(response.statusCode == 201){
+	      console.log('inserted ' + entry.id)
+	    } else {
+	      console.log('error: '+ response.statusCode)
+	      console.log(body)
+	    }
+	  }
+	)	
+}
+
+
+for (var i in json) {
+	var entry = json[i];
+	entry.id = ("" + entry.lat).replace('.','') + ("" + entry.lon).replace('.','');
+	entry.id = crypto.createHash('md5').update(entry.id);
+	entry.id = entry.id.digest('hex')
+	insert(entry);
+}
