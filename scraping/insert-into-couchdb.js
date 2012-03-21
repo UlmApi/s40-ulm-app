77c969e86e13c56b79a3a3a8fee38836731d6d9c
cmichi <mich@elmueller.net>
2012-03-21 22:22:44 +0100

    added script for inserting open times into couchdb

---
+++ scraping/insert-into-couchdb.js
@@ -0,0 +1,37 @@
+/* simple script for inserting into couchdb */
+var request = require('request');
+var fs = require('fs');
+var crypto = require('crypto');
+var credentials = require('./credentials.js');
+
+var json = fs.readFileSync('./restaurants_test.json', 'utf8');
+var json = JSON.parse(json);
+
+
+function insert(entry) {
+	request(
+	  { method: 'PUT'
+	  , uri:  'http://daten.ulmapi.de/oeffnungszeiten/' + entry.id
+	  ,headers: 	{'Authorization': 'Basic ' + credentials.auth}
+	  , multipart: 
+	    [ { 'content-type': 'application/json'
+	      ,  body: JSON.stringify(entry)
+	      }
+	    ] 
+	  }
+	, function (error, response, body) {
+	    if(response.statusCode == 201){
+	      console.log('inserted ' + entry.id)
+	    } else {
+	      console.log('error: '+ response.statusCode)
+	      console.log(body)
+	    }
+	  }
+	)	
+}
+
+
+for (var i in json) {
+	var entry = json[i];
+	insert(entry);
+}
