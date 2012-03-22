a79a24ac951d49ba430a0e43b7226a17c4b3d8af
cmichi <mich@elmueller.net>
2012-03-22 13:14:36 +0100

    up-to-date execute script

--- scraping/execute.sh
+++ scraping/execute.sh
@@ -5,8 +5,8 @@
 #file="restaurants.json"
 #file2="restaurants_with_geo.json"
 
-file="cafes.json"
-file2="cafes_with_geo.json"
+#file="cafes.json"
+#file2="cafes_with_geo.json"
 
 file="clubs.json"
 file2="clubs_with_geo.json"
@@ -18,12 +18,7 @@ sed '$d' < $file > tmp ; mv tmp $file
 sed '$d' < $file > tmp ; mv tmp $file
 echo "]" >> $file
 
-#echo "[" > $file2
 node geocoding.js >> $file2
-# delete last 2 lines
-#sed '$d' < $file2 > tmp ; mv tmp $file2
-#sed '$d' < $file2 > tmp ; mv tmp $file2
-#echo "]" >> $file2
 
-#node insert-into-couchdb.js
+node insert-into-couchdb.js
 
